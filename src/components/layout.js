32da49d85b70401996312162ac2d6138f105dfbf
jackiehyho <jackie.hy.ho@gmail.com>
2021-12-25 21:58:27 -0800

    Extract navigation menu into seperate component

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './variables.css'
 import './global.css'
 import Seo from './seo'
-import Navigation from './navigation'
+import Header from './header'
 import Footer from './footer'
 class Template extends React.Component {
   render() {
@@ -12,7 +12,7 @@ class Template extends React.Component {
     return (
       <>
         <Seo />
-        <Navigation />
+        <Header />
         <main>{children}</main>
         <Footer />
       </>
